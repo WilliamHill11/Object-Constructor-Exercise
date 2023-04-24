3950944c2f98bdc79bdd1bc0c315eedda327c90d
william hill <williamhill0831@Hotmail.com>
2023-04-24 19:01:57 -0400

    Working with different objects and prototypes

--- script.js
+++ script.js
@@ -11,4 +11,40 @@ function Book(title, author, pages, read) {
 
 const theHobbit = new Book('the Hobbit', 'by John Smith', 2039, 'not a chance');
 
-console.log(theHobbit.info());
+// Initialize constructor functions
+function Hero(name, level) {
+  this.name = name;
+  this.level = level;
+}
+
+function Warrior(name, level, weapon) {
+  Hero.call(this, name, level);
+
+  this.weapon = weapon;
+}
+
+function Healer(name, level, spell) {
+  Hero.call(this, name, level);
+
+  this.spell = spell;
+}
+
+// Link prototypes and add prototype methods
+Object.setPrototypeOf(Warrior.prototype, Hero.prototype);
+Object.setPrototypeOf(Healer.prototype, Hero.prototype);
+
+Hero.prototype.greet = function () {
+  return `${this.name} says hello.`;
+};
+
+Warrior.prototype.attack = function () {
+  return `${this.name} attacks with the ${this.weapon}.`;
+};
+
+Healer.prototype.heal = function () {
+  return `${this.name} casts ${this.spell}.`;
+};
+
+// Initialize individual character instances
+const hero1 = new Warrior('Bjorn', 1, 'axe');
+const hero2 = new Healer('Kanin', 1, 'cure');
